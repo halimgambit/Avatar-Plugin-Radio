47fbc4a4aab7660052b516573fe1f6b64dc39a55
halimgambit <70286759+halimgambit@users.noreply.github.com>
2023-11-02 06:16:47 +0100

    Update Radio.js

--- Radio.js
+++ Radio.js
@@ -8,6 +8,9 @@ exports.action = function(data, callback){
 					},
 		abcLounge : function() {
 			stationRadio ("abcLounge", data, client);
+					},
+		europe1 : function() {
+			stationRadio ("europe1", data, client);
 					},				
 		arretRadio : function() {
 			stationRadio ("arretRadio", data, client);
@@ -18,21 +21,25 @@ exports.action = function(data, callback){
 	function stationRadio (radio, data, client) {
 
 		if (radio === "franceInfo") {
-			Avatar.play('%URL%http://icecast.radiofrance.fr/franceinfo-hifi.aac', data.client);
+			Avatar.play(`%URL%${'http://icecast.radiofrance.fr/franceinfo-hifi.aac'}`, client);
 		}
-		if (radio === "nostalgie") {
-			Avatar.play('%URL%https://f5-pa6.nrjaudio.fm/fr/30601/mp3_128.mp3?origine=fluxradios&cdn_path=adswizz_lbs10&access_token=be914175275a4ffa80a42a967f924ccf', data.client);
+		else if (radio === "nostalgie") {
+			Avatar.play(`%URL%${'https://f5-pa6.nrjaudio.fm/fr/30601/mp3_128.mp3?origine=fluxradios&cdn_path=adswizz_lbs10&access_token=be914175275a4ffa80a42a967f924ccf'}`, client);
 		}
-		if (radio === "abcLounge") {
-			Avatar.play('%URL%https://streamingv2.shoutcast.com/JamendoLounge', data.client);
+		else if (radio === "abcLounge") {
+			Avatar.play(`%URL%${'https://streamingv2.shoutcast.com/JamendoLounge'}`, client);
 		}
-		if (radio === "arretRadio") {
+		else if (radio === "europe1") {
+			Avatar.play(`%URL%${'http://stream.europe1.fr/europe1.mp3'}`, client);
+		}
+		else if (radio === "arretRadio") {
 			Avatar.speak("J'arréte la radio", data.client, () => {
-					 Avatar.Speech.end(data.client, true, () => {
-						 Avatar.stop(null, data.client);
-					 });
-				});
+			Avatar.Speech.end(data.client, true, () => {
+			Avatar.stop(null, client);
+			});
+		});
 		}
+		
 	}
 
 
